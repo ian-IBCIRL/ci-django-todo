716c15aae9f0f740ee973c8a4135b48f8232661d
ian-IBCIRL <ian@ibcirl.com>
2022-11-19 17:11:10 +0000

    added user creation and initial models

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Item(models.Model):
+    name = models.CharField(max_length=50, null=False, blank=False)
+    done = models.BooleanField(null=False, blank=False)
+
+def create_or_update_user_profile( sender, instance, created, **kwargs ):
+    """create the user profile 
+
+    Args:
+        sender (_type_): _description_
+        instance (_type_): _description_
+        created (_type_): _description_
+    """
+    UserProfile.objects.create(user=instance)
